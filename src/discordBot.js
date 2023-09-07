c65fb3d8e556eca6552665d38d58a03481f28c5f
JSON-Lee-ai <jayson-lee.gillis@hotmail.be>
2023-09-07 20:48:06 +0200

    changed structure, can send messages via webapp

---
+++ src/discordBot.js
@@ -0,0 +1,57 @@
+// at the top of your file
+const { Client, Events, GatewayIntentBits, EmbedBuilder } = require('discord.js');
+const { token, channel_id } = require('../config.json');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+
+function send_embed() {
+    const channel = client.channels.cache.get(channel_id);
+    console.log(`Channel: ${channel.name}`);
+
+    // inside a command, event listener, etc.
+    const exampleEmbed = new EmbedBuilder()
+    .setColor(0x0099FF)
+    .setTitle('Some title')
+    .setURL('https://discord.js.org/')
+    .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+    .setDescription('Some description here')
+    .setThumbnail('https://i.imgur.com/AfFp7pu.png')
+    .addFields(
+        { name: 'Regular field title', value: 'Some value here' },
+        { name: '\u200B', value: '\u200B' },
+        { name: 'Inline field title', value: 'Some value here', inline: true },
+        { name: 'Inline field title', value: 'Some value here', inline: true },
+    )
+    .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+    .setImage('https://i.imgur.com/AfFp7pu.png')
+    .setTimestamp()
+    .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+
+    channel.send({ embeds: [exampleEmbed] });
+}
+
+function edit_embed() {
+    const channel = client.channels.cache.get(channel_id);
+    console.log(`Channel: ${channel.name}`);
+
+    const embed_id = "1149389626977562744"
+    //get the message by id
+    const message = channel.messages.fetch(embed_id)
+        // .then(message => message.edit({ embeds: [exampleEmbed] }));
+        .then(message => console.log(message.embeds[0].title + "\n" +
+        message.embeds[0].description));
+}
+
+client.once(Events.ClientReady, c => {
+    console.log(`Ready! Logged in as ${c.user.tag}`);
+    
+    send_embed();
+});
+
+
+// Log in to Discord with your client's token
+client.login(token);
+
