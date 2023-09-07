f29466a09782006666a03b54e99b14690d65d5f8
JSON-Lee-ai <jayson-lee.gillis@hotmail.be>
2023-09-07 21:08:06 +0200

    updated structure AGAIN, refactored code ... duh

--- src/app.js
+++ src/app.js
@@ -1,37 +1,23 @@
-const { Client, Events, GatewayIntentBits, EmbedBuilder } = require('discord.js');
-const { token, channel_id } = require('../config.json');
 const express = require('express');
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const path = require('path');
 const app = express();
 const port = 3000;
 
-client.once(Events.ClientReady, c => {
-    console.log(`Ready! Logged in as ${c.user.tag}`);
-});
-
-// Log in to Discord with your client's token
-client.login(token);
-
-function send_message(message) {
-    const channel = client.channels.cache.get(channel_id);
-    console.log(`Channel: ${channel.name}`);
-
-    channel.send(message);
-}
+const { sendMessage } = require('./discordBot');
+const embed_id = "1149389626977562744"
 
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     // Render the HTML form here
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 app.post('/send-data', (req, res) => {
     const { message } = req.body;
     console.log(message);
 
-    send_message(message);
+    sendMessage(message);
     // Redirect the user back to the form page or another page as needed.
     res.redirect('/');
 });
--- src/discordBot.js
+++ src/discordBot.js
@@ -4,10 +4,20 @@ const { token, channel_id } = require('../config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+    console.log(`Ready! Logged in as ${c.user.tag}`);
+});
 
-function send_embed() {
+client.login(token);
+
+function sendMessage(message) {
+    const channel = client.channels.cache.get(channel_id);
+    console.log(`Channel: ${channel.name}`);
+
+    channel.send(message);
+}
+
+function sendEmbed() {
     const channel = client.channels.cache.get(channel_id);
     console.log(`Channel: ${channel.name}`);
 
@@ -16,16 +26,13 @@ function send_embed() {
     .setColor(0x0099FF)
     .setTitle('Some title')
     .setURL('https://discord.js.org/')
-    .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
     .setDescription('Some description here')
     .setThumbnail('https://i.imgur.com/AfFp7pu.png')
     .addFields(
         { name: 'Regular field title', value: 'Some value here' },
-        { name: '\u200B', value: '\u200B' },
         { name: 'Inline field title', value: 'Some value here', inline: true },
         { name: 'Inline field title', value: 'Some value here', inline: true },
     )
-    .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
     .setImage('https://i.imgur.com/AfFp7pu.png')
     .setTimestamp()
     .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
@@ -33,25 +40,15 @@ function send_embed() {
     channel.send({ embeds: [exampleEmbed] });
 }
 
-function edit_embed() {
+function editEmbed(message_id) {
     const channel = client.channels.cache.get(channel_id);
     console.log(`Channel: ${channel.name}`);
 
-    const embed_id = "1149389626977562744"
     //get the message by id
-    const message = channel.messages.fetch(embed_id)
+    const message = channel.messages.fetch(message_id)
         // .then(message => message.edit({ embeds: [exampleEmbed] }));
         .then(message => console.log(message.embeds[0].title + "\n" +
         message.embeds[0].description));
 }
 
-client.once(Events.ClientReady, c => {
-    console.log(`Ready! Logged in as ${c.user.tag}`);
-    
-    send_embed();
-});
-
-
-// Log in to Discord with your client's token
-client.login(token);
-
+module.exports = { sendMessage, sendEmbed, editEmbed };
